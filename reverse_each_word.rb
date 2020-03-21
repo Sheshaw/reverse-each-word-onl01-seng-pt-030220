546add022d377725ccd4860afa7d9311af1b7eda
Chayan Martinez <nayachmartinez68@gmail.com>
2020-03-21 23:45:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sent)
   new_sent = sent.split(" ")
-  new_sent.each do |word|
+  new_sent_2 = new_sent.map do |word|
     word.reverse
-  end
-  new_sent
+   end
+   new_sent_2.join(" ")
 end
